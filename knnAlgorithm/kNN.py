c28a2f86fb83cea06562e6d8b27c44554d02e595
endermeihl <endermeihl@gmail.com>
2018-01-11 09:57:55 +0800

    vim学习

---
+++ knnAlgorithm/kNN.py
@@ -0,0 +1,10 @@
+from numpy import *
+import operator
+
+def createDateSet():
+    group = ([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]

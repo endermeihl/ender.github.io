64df8b41d6fb321b0c743b5cf9596681a121dfd3
endermeihl <endermeihl@gmail.com>
2018-01-17 14:41:16 +0800

    学习尝试

---
+++ demo/01/confirm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+age = 30
+if age >= 18:
+    print('your age is', age)
+    print('adult')
+    
---
+++ demo/01/list.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+print(classmates[-1])
+classmates[1]='ender'
+print(classmates)
+classmates.append('Mei')
+print(classmates)
+classmates.pop()
+print(classmates)
+print(len(classmates))

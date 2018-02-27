a99047a75443d81e2fa8d17ef18c8abe188236b6
endermeihl <endermeihl@gmail.com>
2018-02-27 17:31:44 +0800

    k

--- knnAlgorithm/kNN.py
+++ knnAlgorithm/kNN.py
@@ -1,5 +1,7 @@
-from numpy import *
+from numpy import array, tile, zeros, shape
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 
 def createDateSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -17,10 +19,71 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteIlabel=labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    sortedClassCount = sorted(
+        classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 def file2matrix(filename):
     fr = open(filename)
     arrayOLines = fr.readlines()
-    
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:]=listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDateMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDateMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount=0.0
+    for i in range(numTestVecs):
+        classifierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print("the classifier came back with: %d,the real answer is: %d" %(classifierResult,datingLabels[i]))
+        if(classifierResult!= datingLabels[i]):
+            errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+
+
+datingClassTest()
+
+def datingDeom():
+    datingDateMat, datingLabels = file2matrix('datingTestSet2.txt')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDateMat[:, 0], datingDateMat[:, 2],
+            15.0 * array(datingLabels), 15.0 * array(datingLabels))
+    plt.show()
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing vider games?"))
+    ffMiles = float(input("frequent flier miles earnde per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDateMat,datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDateMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print("You will perbably like this person: ", resultList[classifierResult - 1])
+
+
+classifyPerson()
